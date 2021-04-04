99f7784eb4e8712250b2c40237a04a15d1fc2644
Adam K <adam@kecskes.net>
2021-04-04 06:58:38 -0500

    Added details for the services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -5,43 +5,34 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './styles.module.css';
 import Feature from '../components/Feature'
-import features from '../content/features'
+import services from '../content/services'
 
-export default function Home() {
+export default function Services() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Improve you company's effectiveness with less effort and better results.">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      description="Make great strides in improving your Product Life Cycle, Software Development and Information Architecture ">
+      {/* <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <div className={clsx("twoColumnWrapper", styles.heroFeatures)}>
             <div>
               <div className={clsx(styles.heroFeatures, 'heroText')}>
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
-                {/* <div className={styles.buttons}>
-                  <Link
-                    className={clsx(styles.getStarted,
-                      'button button--outline button--secondary button--lg',
-                    )}
-                    to={useBaseUrl('docs/')}>
-                    Get in Touch
-                  </Link>
-                </div> */}
               </div>
             </div>
             <div><img className={styles.heroIcon} src="img/flipped_imbr_icon.svg" /></div>
           </div>
         </div>
-      </header>
+      </header> */}
       <main>
-        {features && features.length > 0 && (
+        {services && services.length > 0 && (
           <section className={styles.features}>
             <div className="container">
               <div className="row">
-                {features.map((props, idx) => (
+                {services.map((props, idx) => (
                   <Feature key={idx} {...props} />
                 ))}
               </div>
