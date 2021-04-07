e85584f0f9a2a5ebe3e86efa543f4ce51f3dfef4
Adam K <adam@kecskes.net>
2021-04-06 19:37:36 -0500

    Add subtitle component

---
+++ src/components/Subtitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Subtitle = ({ children }) => (
+    <span style={{ display: 'block' }}><strong style={{ fontSize: '2em' }}><em>{children}</em></strong ></span >
+
+);
+
+export default Subtitle
+
